ebfc1bd11bcf43978da5f28e8f81f504e48b82a4
cassiusbessa <cassiusbessa@gmail.com>
2023-01-05 04:42:16 -0300

    test: :white_check_mark: ensure that it is possible to create a address

---
+++ back-end/src/app/entities/address.spec.ts
@@ -0,0 +1,38 @@
+import { Address, AddressProps } from './address';
+
+const anyAddressProps: AddressProps = {
+  street: 'any_street',
+  number: 'any_number',
+  complement: 'any_complement',
+  neighborhood: 'any_neighborhood',
+  city: 'any_city',
+  state: 'any_state',
+  country: 'any_country',
+  zipcode: 'any_zipcode',
+};
+
+interface SutTypes {
+  sut: Address;
+}
+
+const makeSut = (AddressProps: AddressProps = anyAddressProps): SutTypes => {
+  const sut = new Address(AddressProps);
+  return { sut };
+};
+
+describe('Address', () => {
+  it('1 - should be able to create a Address', () => {
+    const sutProps: AddressProps = {
+      street: 'valid_street',
+      number: 'valid_number',
+      complement: 'valid_complement',
+      neighborhood: 'valid_neighborhood',
+      city: 'valid_city',
+      state: 'valid_state',
+      country: 'valid_country',
+      zipcode: 'valid_zipcode',
+    };
+    const sut = new Address(sutProps);
+    expect(sut).toBeTruthy();
+  });
+});
