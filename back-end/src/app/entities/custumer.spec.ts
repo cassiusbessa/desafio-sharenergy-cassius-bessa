44f5f36e7ac5fe41e4a0968c8731032f7c6ae22f
cassiusbessa <cassiusbessa@gmail.com>
2023-01-04 22:53:14 -0300

    refactor: :art: implement emailValidator and sut factory

--- back-end/src/app/entities/custumer.spec.ts
+++ back-end/src/app/entities/custumer.spec.ts
@@ -1,27 +1,47 @@
-import { Custumer } from './custumer';
+import { Custumer, CustumerProps } from './custumer';
+import { EmailValidator } from '../protocols/email-validator';
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(_email: string): boolean {
+      return true;
+    }
+  }
+  return new EmailValidatorStub();
+};
+
+interface SutTypes {
+  sut: Custumer;
+  emailValidatorStub: EmailValidator;
+}
+
+const makeSut = (custumerProps: CustumerProps): SutTypes => {
+  const emailValidatorStub = makeEmailValidator();
+  const sut = new Custumer(custumerProps, emailValidatorStub);
+  return { sut, emailValidatorStub };
+};
 
 describe('Custumer', () => {
   it('1 - should be able to create a custumer', () => {
-    const custumer = new Custumer({
+    const sutProps: CustumerProps = {
       name: 'valid_name',
       email: 'valid_email',
       phone: 'valid_phone',
       cpf: 'valid_cpf',
       address: 'valid_address',
-    });
-    expect(custumer).toBeTruthy();
+    };
+    const { sut } = makeSut(sutProps);
+    expect(sut).toBeTruthy();
   });
 
   it('2 - should not be able to create a custume name with less than 3 characters', () => {
-    expect(
-      () =>
-        new Custumer({
-          name: '12',
-          email: 'valid_email',
-          phone: 'valid_phone',
-          cpf: 'valid_cpf',
-          address: 'valid_address',
-        }),
-    ).toThrowError();
+    const sutProps: CustumerProps = {
+      name: 'na',
+      email: 'valid_email',
+      phone: 'valid_phone',
+      cpf: 'valid_cpf',
+      address: 'valid_address',
+    };
+    expect(() => new Custumer(sutProps, makeEmailValidator())).toThrowError();
   });
 });
