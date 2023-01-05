5741f711275e7d7cba209f85bf845ad0c0a4fd57
cassiusbessa <cassiusbessa@gmail.com>
2023-01-05 18:41:17 -0300

    feat: :sparkles: implement customer repository

---
+++ back-end/src/app/repositories/customer-repository.ts
@@ -0,0 +1,9 @@
+import { Customer } from '../entities/customer';
+
+export abstract class CustomerRepository {
+  abstract register(customer: Customer): Promise<void>;
+  abstract getInfo(id: string): Promise<Customer | null>;
+  abstract update(customer: Customer): Promise<void>;
+  abstract delete(id: string): Promise<void>;
+  abstract getAll(): Promise<Customer[]>;
+}
