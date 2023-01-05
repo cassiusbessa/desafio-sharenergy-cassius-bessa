c223932b37fecdfae3db1dc7c77ec3d002d2ad93
cassiusbessa <cassiusbessa@gmail.com>
2023-01-05 17:04:16 -0300

    refactor: :art: turn sutFactory into a class

--- back-end/src/app/entities/address.spec.ts
+++ back-end/src/app/entities/address.spec.ts
@@ -15,10 +15,20 @@ interface SutTypes {
   sut: Address;
 }
 
-const makeSut = (AddressProps: AddressProps = anyAddressProps): SutTypes => {
-  const sut = new Address(AddressProps);
-  return { sut };
-};
+// const makeSut = (AddressProps: AddressProps = anyAddressProps): SutTypes => {
+//   const sut = new Address(AddressProps);
+//   return { sut };
+// };
+
+class SutFactory {
+  public sutProps: AddressProps;
+  constructor(sutProps: AddressProps) {
+    this.sutProps = sutProps;
+  }
+  makeSut(): Address {
+    return new Address(this.sutProps);
+  }
+}
 
 describe('Address', () => {
   it('1 - should be able to create a Address', () => {
@@ -32,34 +42,38 @@ describe('Address', () => {
       country: 'valid_country',
       zipcode: 'valid_zipcode',
     };
-    const sut = new Address(sutProps);
+    const sut = new SutFactory(sutProps).makeSut();
     expect(sut).toBeTruthy();
   });
 
   it("2 - shouldn't be able to create a Address with street less than 1 characters", () => {
     const sutProps: AddressProps = { ...anyAddressProps, street: '' };
-    expect(() => new Address(sutProps)).toThrowError(
+    const sut = new SutFactory(sutProps);
+    expect(() => sut.makeSut()).toThrowError(
       'Street must have at least 1 character',
     );
   });
 
   it("3 - shouldn't be able to create a Address with number less than 1 characters", () => {
     const sutProps: AddressProps = { ...anyAddressProps, number: '' };
-    expect(() => new Address(sutProps)).toThrowError(
+    const sut = new SutFactory(sutProps);
+    expect(() => sut.makeSut()).toThrowError(
       'Number must have at least 1 character',
     );
   });
 
   it("4 - shouldn't be able to create a Address with neighborhood less than 3 characters", () => {
     const sutProps: AddressProps = { ...anyAddressProps, neighborhood: '12' };
-    expect(() => new Address(sutProps)).toThrowError(
+    const sut = new SutFactory(sutProps);
+    expect(() => sut.makeSut()).toThrowError(
       'Neighborhood must have at least 3 characters',
     );
   });
 
   it("5 - shouldn't be able to create a Address with city less than 3 characters", () => {
     const sutProps: AddressProps = { ...anyAddressProps, city: '12' };
-    expect(() => new Address(sutProps)).toThrowError(
+    const sut = new SutFactory(sutProps);
+    expect(() => sut.makeSut()).toThrowError(
       'City must have at least 3 characters',
     );
   });
@@ -73,20 +87,22 @@ describe('Address', () => {
 
   it("7 - shouldn't be able to create a Address with country less than 3 characters", () => {
     const sutProps: AddressProps = { ...anyAddressProps, country: '12' };
-    expect(() => new Address(sutProps)).toThrowError(
+    const sut = new SutFactory(sutProps);
+    expect(() => sut.makeSut()).toThrowError(
       'Country must have at least 3 characters',
     );
   });
 
   it("8 - shouldn't be able to create a Address with zipcode less than 8 characters", () => {
     const sutProps: AddressProps = { ...anyAddressProps, zipcode: '1234567' };
-    expect(() => new Address(sutProps)).toThrowError(
+    const sut = new SutFactory(sutProps);
+    expect(() => sut.makeSut()).toThrowError(
       'Zipcode must have at least 8 characters',
     );
   });
 
   it('9 - should be able to get the full address', () => {
-    const { sut } = makeSut();
+    const sut = new SutFactory(anyAddressProps).makeSut();
     expect(sut.getFullAddress()).toBe(
       'any_street, any_number, any_complement, any_neighborhood, any_city, any_state, any_country, any_zipcode',
     );
