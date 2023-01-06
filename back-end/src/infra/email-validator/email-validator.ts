461fb6aaece6750df3488ce464e914bff261fcd3
cassiusbessa <cassiusbessa@gmail.com>
2023-01-05 22:02:10 -0300

    refactor: :zap: improve regex pattern

--- back-end/src/infra/email-validator/email-validator.ts
+++ back-end/src/infra/email-validator/email-validator.ts
@@ -3,7 +3,7 @@ import { EmailValidator } from 'src/app/protocols';
 export class EmailValidatorAdapter implements EmailValidator {
   isValid(email: string): boolean {
     const pattern = new RegExp(
-      '^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$',
+      '^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$',
     );
     return pattern.test(email);
   }
