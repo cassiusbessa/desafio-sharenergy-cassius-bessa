e4a9fa6585b2a979534669573441e45b29b96306
cassiusbessa <cassiusbessa@gmail.com>
2023-01-06 05:00:00 -0300

    refactor: :art: change getInfo returns to all customer props and id

--- back-end/src/domain/repositories/customer-repository.ts
+++ back-end/src/domain/repositories/customer-repository.ts
@@ -2,7 +2,9 @@ import { Customer, CustomerProps } from '../entities/customer';
 
 export abstract class CustomerRepository {
   abstract register(customer: CustomerProps & { id: string }): Promise<void>;
-  abstract getInfo(id: string): Promise<Customer | null>;
+  abstract getInfo(
+    id: string,
+  ): Promise<(CustomerProps & { id: string }) | null>;
   abstract update(customer: CustomerProps, id: string): Promise<void>;
   abstract delete(id: string): Promise<void>;
   abstract getAll(): Promise<Customer[]>;
