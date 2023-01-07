83110388b55c949eae1b2de5c2eded8da0502d71
cassiusbessa <cassiusbessa@gmail.com>
2023-01-07 03:19:28 -0300

    refactor: :art: change returns types

--- back-end/src/domain/repositories/customer-repository.ts
+++ back-end/src/domain/repositories/customer-repository.ts
@@ -1,11 +1,11 @@
 import { Customer, CustomerProps } from '../entities/customer';
 
 export abstract class CustomerRepository {
-  abstract register(customer: CustomerProps & { id: string }): Promise<void>;
+  abstract register(customer: CustomerProps & { id: string }): Promise<boolean>;
   abstract getInfo(
     id: string,
   ): Promise<(CustomerProps & { id: string }) | null>;
-  abstract update(customer: CustomerProps, id: string): Promise<void>;
-  abstract delete(id: string): Promise<void>;
+  abstract update(customer: CustomerProps, id: string): Promise<boolean>;
+  abstract delete(id: string): Promise<boolean>;
   abstract getAll(): Promise<CustomerProps & { id: string }[]>;
 }
