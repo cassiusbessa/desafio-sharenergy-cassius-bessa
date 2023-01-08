b628f9ca6c1a96886e57f49612f9f85b8c7568ea
cassiusbessa <cassiusbessa@gmail.com>
2023-01-08 02:45:40 -0300

    test: :white_check_mark: ensure that throws if CustomerRepository throws

--- back-end/src/data/use-cases/db-customer-use-cases/db-get-all-customer/db-get-all-customer.spec.ts
+++ back-end/src/data/use-cases/db-customer-use-cases/db-get-all-customer/db-get-all-customer.spec.ts
@@ -17,4 +17,12 @@ describe('DbGetAllCustomer', () => {
     await sut.getAll();
     expect(getAllSpy).toHaveBeenCalled();
   });
+
+  it('2 - should throw if CustomerRepository throws', async () => {
+    const { sut, customerRepositoryStub } = makeSut();
+    jest
+      .spyOn(customerRepositoryStub, 'getAll')
+      .mockReturnValueOnce(Promise.reject(new Error()));
+    expect(sut.getAll()).rejects.toThrow();
+  });
 });
