251f5b4f8ef3c1968725572b43f45f97aeca3661
cassiusbessa <cassiusbessa@gmail.com>
2023-01-08 01:17:48 -0300

    refactor: :fire: remove unnecessary codes

--- back-end/src/data/use-cases/db-customer-use-cases/db-register-customer/db-register-customer.ts
+++ back-end/src/data/use-cases/db-customer-use-cases/db-register-customer/db-register-customer.ts
@@ -1,20 +1,16 @@
-import { CustomerProps, PersistenceCustomer } from '@domain/entities/customer';
+import { PersistenceCustomer } from '@domain/entities/customer';
 import { CustomerRepository } from '@domain/repositories/customer-repository';
 import { RegisterCustomer } from '@domain/use-cases/customer-use-cases/register-customer';
-import { makeCustomer } from '../../../../factories/makeCustomer';
 
 export class DbRegisterCustomer implements RegisterCustomer {
   constructor(private readonly customerRepository: CustomerRepository) {}
 
   async register(customer: PersistenceCustomer): Promise<boolean> {
     const exists = await this.customerRepository.getByEmail(customer.email);
-    console.log('exists: ', exists);
     if (exists) {
       return false;
     }
-    console.log('vou registar o customer: ', customer);
     const isValid = await this.customerRepository.register(customer);
-    console.log('isValid: ', isValid);
     return isValid;
   }
 }
