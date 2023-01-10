daf1d1281c04c63154f69d6ada65c08c0764306d
cassiusbessa <cassiusbessa@gmail.com>
2023-01-10 04:50:42 -0300

    refactor: :art: export new protocols from index

--- back-end/src/domain/protocols/index.ts
+++ back-end/src/domain/protocols/index.ts
@@ -2,3 +2,5 @@ export * from './email-validator';
 export * from './phone-validator';
 export * from './cpf-validator';
 export * from './validator-result';
+export * from './customer-validator';
+export * from './customer-factory';
