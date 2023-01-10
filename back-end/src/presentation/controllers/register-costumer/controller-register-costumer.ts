f801fd90a7e2e5bfb8dc9264a7f2547d17334ec1
cassiusbessa <cassiusbessa@gmail.com>
2023-01-10 04:53:36 -0300

    feat: :sparkles: ensure that calls create entitie with param from http body

--- back-end/src/presentation/controllers/register-costumer/controller-register-costumer.ts
+++ back-end/src/presentation/controllers/register-costumer/controller-register-costumer.ts
@@ -1,8 +1,19 @@
 import { MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-helper';
 import { Controller, HttpRequest } from 'src/presentation/protocols';
+import { RegisterCustomer } from '@domain/use-cases/customer-use-cases/register-customer';
+import { CustomerFactory } from '@domain/protocols';
 
 export class ControllerRegisterCustomer implements Controller {
+  private readonly registerCustomer: RegisterCustomer;
+  customerFactory: CustomerFactory;
+  constructor(
+    registerCustomer: RegisterCustomer,
+    customerFactory: CustomerFactory,
+  ) {
+    this.registerCustomer = registerCustomer;
+    this.customerFactory = customerFactory;
+  }
   async handle(httpRequest: HttpRequest) {
     const requiredFields = ['name', 'email', 'phone', 'cpf', 'address'];
     for (const field of requiredFields) {
@@ -10,6 +21,14 @@ export class ControllerRegisterCustomer implements Controller {
         return badRequest(new MissingParamError(field));
       }
     }
+    const { name, email, phone, cpf, address } = httpRequest.body;
+    const { customer } = this.customerFactory.create({
+      name,
+      email,
+      phone,
+      cpf,
+      address,
+    });
     return { statusCode: 201, body: { message: 'created' } };
   }
 }
