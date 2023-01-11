e1373763d503ba32d48fb13d58a1c822b611712b
cassiusbessa <cassiusbessa@gmail.com>
2023-01-11 02:32:43 -0300

    test: :white_check_mark: ensure that calls static create method with correct values

--- back-end/src/data/use-cases/db-customer-use-cases/db-register-customer/db-register-customer.spec.ts
+++ back-end/src/data/use-cases/db-customer-use-cases/db-register-customer/db-register-customer.spec.ts
@@ -1,15 +1,22 @@
+import { Customer } from '@domain/entities/customer/customer';
 import { defaultPersistenceCustomer } from '@tests/customer/mocks/entities/default-entitie.mock';
 import { makeDbRegisterCustomerMock as makeSut } from '@tests/customer/mocks/use-cases/db-register-customer.mock';
 
 describe('DbRegisterCustomer', () => {
-  it('1 - should call CustomerRepository with correct values', async () => {
+  it('1 - should call static create method with correct values', async () => {
+    const { sut } = makeSut();
+    const createSpy = jest.spyOn(Customer, 'create');
+    await sut.register(defaultPersistenceCustomer);
+    expect(createSpy).toHaveBeenCalledWith(defaultPersistenceCustomer);
+  });
+  it('2 - should call CustomerRepository with correct values', async () => {
     const { sut, customerRepositoryStub } = makeSut();
     const registerSpy = jest.spyOn(customerRepositoryStub, 'register');
     await sut.register(defaultPersistenceCustomer);
     expect(registerSpy).toHaveBeenCalledWith(defaultPersistenceCustomer);
   });
 
-  it('2 - should throw if CustomerRepository throws', async () => {
+  it('3 - should throw if CustomerRepository throws', async () => {
     const { sut, customerRepositoryStub } = makeSut();
     jest
       .spyOn(customerRepositoryStub, 'register')
@@ -17,7 +24,7 @@ describe('DbRegisterCustomer', () => {
     expect(sut.register(defaultPersistenceCustomer)).rejects.toThrow();
   });
 
-  it('3 - should return false on if email already exists', async () => {
+  it('4 - should return false on if email already exists', async () => {
     const { sut, customerRepositoryStub } = makeSut();
     jest
       .spyOn(customerRepositoryStub, 'register')
@@ -26,7 +33,7 @@ describe('DbRegisterCustomer', () => {
     expect(isValid).toBe(false);
   });
 
-  it('4 - should return true on success', async () => {
+  it('5 - should return true on success', async () => {
     const { sut } = makeSut();
     const isValid = await sut.register(defaultPersistenceCustomer);
     expect(isValid).toBe(true);
