877db250a1c07dd5e8cf32a5b8547cf30121a852
cassiusbessa <cassiusbessa@gmail.com>
2023-01-11 02:32:20 -0300

    feat: :sparkles: ensure that calls static create method with correct values

--- back-end/src/data/use-cases/db-customer-use-cases/db-register-customer/db-register-customer.ts
+++ back-end/src/data/use-cases/db-customer-use-cases/db-register-customer/db-register-customer.ts
@@ -1,11 +1,16 @@
-import { PersistenceCustomer } from '@domain/entities/customer/customer';
+import {
+  Customer,
+  CustomerProps,
+  PersistenceCustomer,
+} from '@domain/entities/customer/customer';
 import { CustomerRepository } from '@domain/repositories/customer-repository';
 import { RegisterCustomer } from '@domain/use-cases/customer-use-cases/register-customer';
 
 export class DbRegisterCustomer implements RegisterCustomer {
   constructor(private readonly customerRepository: CustomerRepository) {}
 
-  async register(customer: PersistenceCustomer): Promise<boolean> {
+  async register(customerProps: CustomerProps): Promise<boolean> {
+    const customer = Customer.create(customerProps).getAllProps();
     const isValid = await this.customerRepository.register(customer);
     return isValid;
   }
