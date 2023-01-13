c7734ccb7ec914af5059b26184d920a26db700db
cassiusbessa <cassiusbessa@gmail.com>
2023-01-13 04:03:23 -0300

    fix: :bug: fix status codes returns

--- back-end/src/infra/nest/http/nest-controllers/nest-controller-register-customer.ts
+++ back-end/src/infra/nest/http/nest-controllers/nest-controller-register-customer.ts
@@ -1,11 +1,12 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Controller, Post, Req, Res } from '@nestjs/common';
 import { ControllerRegisterCustomer } from '@presentation/controllers/register-costumer/controller-register-costumer';
 import { HttpRequest } from '@presentation/protocols';
 
 @Controller('/customers')
 export class NestControllerRegisterCustomer extends ControllerRegisterCustomer {
   @Post()
-  async handle(@Req() httpRequest: HttpRequest) {
-    return super.handle(httpRequest);
+  async handle(@Req() httpRequest: HttpRequest, @Res() res: any) {
+    const result = await super.handle(httpRequest);
+    return res.status(result.statusCode).json(result.body);
   }
 }
--- back-end/src/infra/nest/http/nest-controllers/nest-controller-update-customer.ts
+++ back-end/src/infra/nest/http/nest-controllers/nest-controller-update-customer.ts
@@ -1,11 +1,15 @@
-import { Controller, Put, Req } from '@nestjs/common';
+import { Controller, Put, Req, Res } from '@nestjs/common';
 import { ControllerUpdateCustomer } from '@presentation/controllers/update-customer/controller-update-customer';
-import { HttpRequest } from '@presentation/protocols';
+import { HttpRequest, HttpResponse } from '@presentation/protocols';
 
 @Controller('/customers')
 export class NestControllerUpdateCustomer extends ControllerUpdateCustomer {
   @Put()
-  async handle(@Req() httpRequest: HttpRequest) {
-    return super.handle(httpRequest);
+  async handle(
+    @Req() httpRequest: HttpRequest,
+    @Res() res: any,
+  ): Promise<HttpResponse> {
+    const result = await super.handle(httpRequest);
+    return res.status(result.statusCode).json(result.body);
   }
 }
