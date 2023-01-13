92ed8efbfd1334ebf7ea9c6ba7b1bb1c1d96dacf
cassiusbessa <cassiusbessa@gmail.com>
2023-01-12 23:44:10 -0300

    feat: :sparkles: implement index

---
+++ back-end/src/infra/nest/http/nest-controllers/index.ts
@@ -0,0 +1,4 @@
+export * from './nest-controller-register-customer';
+export * from './nest-controller-delete-customer';
+export * from './nest-controller-get-all-customer';
+export * from './nest-controller-update-customer';
