81f8ac0aedc5f9abd63a1351d9675679229f0ad0
cassiusbessa <cassiusbessa@gmail.com>
2023-01-13 03:23:59 -0300

    fix: :bug: add container mongo port in db uri

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,11 +1,13 @@
+import { HttpModule } from '@infra/nest/http/modules/http.module';
+import { DatabaseModule } from '@infra/nest/mongo/database.module';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/ShareEnergy')],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/ShareEnergy'),
+    HttpModule,
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
