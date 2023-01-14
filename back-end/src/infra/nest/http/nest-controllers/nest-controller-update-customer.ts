64e266ec81ac3f76adb683cd53a6925c0c0b3c2c
cassiusbessa <cassiusbessa@gmail.com>
2023-01-14 00:02:13 -0300

    fix: :bug: implement url param id and inject dependencies

--- back-end/src/infra/nest/http/nest-controllers/nest-controller-update-customer.ts
+++ back-end/src/infra/nest/http/nest-controllers/nest-controller-update-customer.ts
@@ -1,10 +1,23 @@
-import { Controller, Put, Req, Res } from '@nestjs/common';
+import { CustomerValidator, AddressValidator } from '@domain/protocols';
+import { RegisterCustomer } from '@domain/use-cases/customer-use-cases/register-customer';
+import { UpdateCustomer } from '@domain/use-cases/customer-use-cases/update-customer';
+import { Controller, Inject, Put, Req, Res } from '@nestjs/common';
 import { ControllerUpdateCustomer } from '@presentation/controllers/update-customer/controller-update-customer';
 import { HttpRequest, HttpResponse } from '@presentation/protocols';
 
 @Controller('/customers')
 export class NestControllerUpdateCustomer extends ControllerUpdateCustomer {
-  @Put()
+  constructor(
+    @Inject('UpdateCustomer')
+    updateCustomer: UpdateCustomer,
+    @Inject('CustomerValidator')
+    customerValidator: CustomerValidator,
+    @Inject('AddressValidator')
+    addressValidator: AddressValidator,
+  ) {
+    super(updateCustomer, customerValidator, addressValidator);
+  }
+  @Put('/:id')
   async handle(
     @Req() httpRequest: HttpRequest,
     @Res() res: any,
