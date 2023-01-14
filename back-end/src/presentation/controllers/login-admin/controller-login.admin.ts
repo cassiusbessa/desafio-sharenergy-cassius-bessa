cb8751bd53c024938353b6f628e2f051f44a01a4
cassiusbessa <cassiusbessa@gmail.com>
2023-01-14 04:04:40 -0300

    feat: :sparkles: ensure that returns 500 if any method throws

--- back-end/src/presentation/controllers/login-admin/controller-login.admin.ts
+++ back-end/src/presentation/controllers/login-admin/controller-login.admin.ts
@@ -1,3 +1,4 @@
+import { serverError } from './../../helpers/http-helper';
 import { unauthorized } from '@presentation/helpers/http-helper';
 import { LoginValidator } from '@domain/protocols';
 import { LoginAdmin } from '@domain/use-cases/admin-use-cases/login-admin';
@@ -14,17 +15,21 @@ export class ControllerLoginAdmin implements Controller {
     this.loginValidator = adminValidator;
   }
   async handle(httpRequest: HttpRequest, httpResponse?: HttpResponse) {
-    const requiredFields = ['email', 'password'];
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const requiredFields = ['email', 'password'];
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
+      const { email, password } = httpRequest.body;
+      const isValid = this.loginValidator.validate(email, password);
+      if (!isValid) {
+        return unauthorized(new UnauthorizedError());
+      }
+      const accessToken = await this.loginAdmin.login(email, password);
+    } catch (error) {
+      return serverError();
     }
-    const { email, password } = httpRequest.body;
-    const isValid = this.loginValidator.validate(email, password);
-    if (!isValid) {
-      return unauthorized(new UnauthorizedError());
-    }
-    const accessToken = await this.loginAdmin.login(email, password);
   }
 }
