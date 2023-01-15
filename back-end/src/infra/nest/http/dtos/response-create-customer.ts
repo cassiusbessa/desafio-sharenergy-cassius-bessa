3ff183909086fadfcba78eacb64d22608b3be70e
cassiusbessa <cassiusbessa@gmail.com>
2023-01-15 03:58:25 -0300

    fix: :bug: use nest/swagger decorators correctly

--- back-end/src/infra/nest/http/dtos/response-create-customer.ts
+++ back-end/src/infra/nest/http/dtos/response-create-customer.ts
@@ -1,28 +1,31 @@
 import { ApiProperty } from '@nestjs/swagger';
+
 export class ResponseCreateCustomer {
   @ApiProperty({
-    description: 'Cliente criado',
+    description: 'Mensagem de sucesso',
+    example: 'Customer registered successfully',
+  })
+  message: string;
+
+  @ApiProperty({
+    description: 'Dados do cliente',
     example: {
-      message: 'Customer registered successfully',
-      data: {
-        id: 'c9b5e8a0-5b9f-4b9c-9c9c-0c9c9c9c9c9c',
-        name: 'Cássius Bessa',
-        email: 'cassius@email.com',
-        phone: '+5511999999999',
-        cpf: '123.456.789-00',
-        address: {
-          street: 'Rua dos Bobos',
-          number: '0',
-          complement: 'Apto 0',
-          city: 'São Paulo',
-          state: 'SP',
-          country: 'Brasil',
-          zipcode: '00000-000',
-        },
+      id: '1',
+      name: 'Cássius Bessa',
+      email: 'cassius@email.com',
+      phone: '+5511999999999',
+      cpf: '123.456.789-00',
+      address: {
+        street: 'Rua dos Bobos',
+        number: '0',
+        complement: 'Apto 0',
+        city: 'São Paulo',
+        state: 'SP',
+        country: 'Brasil',
+        zipcode: '00000-000',
       },
     },
   })
-  message: string;
   data: {
     id: string;
     name: string;
