b2dfa80c3af2532d51860957d540eae7f28470b4
cassiusbessa <cassiusbessa@gmail.com>
2023-01-14 23:41:12 -0300

    refactor: :fire: remove not used codes

--- back-end/src/domain/protocols/token-service.ts
+++ back-end/src/domain/protocols/token-service.ts
@@ -4,6 +4,5 @@ export interface TokenPayload {
 }
 
 export interface TokenService {
-  generateToken: (payload: TokenPayload) => Promise<string>;
   verifyToken: (token: string) => Promise<TokenPayload | false>;
 }
