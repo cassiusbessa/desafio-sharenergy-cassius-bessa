707ca4ad07ceae2b058aeb374abdba184b047b77
cassiusbessa <cassiusbessa@gmail.com>
2023-01-16 03:51:11 -0300

    refactor: :art: join get session token and local storage token

--- front-end/src/apis/main-api/auth.ts
+++ front-end/src/apis/main-api/auth.ts
@@ -1,11 +1,9 @@
-export const getToken = () => localStorage.getItem('@token');
+export const getToken = () => localStorage.getItem('@token') || sessionStorage.getItem('@token');
 
 export const setToken = (token: string) => localStorage.setItem('@token', token);
 
 export const clearToken = () => localStorage.removeItem('@token');
 
 export const setSessionToken = (token: string) => sessionStorage.setItem('@token', token);
-
-export const getSessionToken = () => sessionStorage.getItem('@token');
 
 export const clearSessionToken = () => sessionStorage.removeItem('@token'); 
